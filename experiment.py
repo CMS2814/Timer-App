afca2631de0f8dcd70bc985d6ebc36580adbf07f
CMS2814 <cydhensabate14@gmail.com>
2025-12-02 19:24:32 -0500

    printing timer in terminal

--- experiment.py
+++ experiment.py
@@ -7,14 +7,30 @@ from PyQt6.QtWidgets import (
     QWidget,
     QLabel,
 )
+from PyQt6.QtGui import QFont
+from PyQt6.QtCore import QTimer
 
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
+        self.time = 60
 
         self.setWindowTitle("Timey")
-        self.setGeometry(100, 100, 500, 500)
+        self.setGeometry(700, 300, 500, 500)
+
+        label = QLabel(str(self.time), self)
+        label.setGeometry(0, 0, 230, 100)
+        label.setFont(QFont("Arial", 40))
+        label.setStyleSheet("color: blue;" "background-color: red;")
+
+        self.timer = QTimer()
+        self.timer.timeout.connect(self.timer_tick)
+        self.timer.start(1000)  # Timer will tick every 1000 milliseconds (1 second)
+
+    def timer_tick(self):
+        print(self.time)
+        self.time -= 1
 
 
 def main():
