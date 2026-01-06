19f323fb1b04c50ea386a7665ab5afd530a3c47d
CMS2814 <cydhensabate14@gmail.com>
2026-01-06 12:01:42 -0500

    basically has all the basic functionality learned delta time so now i gotta use it for the timer instead of this nonsense

--- Logic/Timer.py
+++ Logic/Timer.py
@@ -9,52 +9,61 @@ class Stopwatch:
     def __init__(self):
         self._time = 0
         self.isGoing = True
+        self.isReset = True
         self.stopWatch = ""
         self.text = time.strftime("%H:%M:%S", time.gmtime(self._time))
 
     def start(self):
-        self.isGoing = True
-        self._time = 1
-        self.update()
+        if self.isReset:
+            self.isReset = False
+            print(self.isReset)
+            self.isGoing = True
+            self.update()
+            print("Stopwatch started")
 
     def stop(self):
         self.isGoing = False
+        print("Stopwatch stopped")
 
     def resume(self):
         self.isGoing = True
+        print("Stopwatch resumed")
 
     def update(self):
-        while self._time > 0:
+        while True:
             if self.isGoing:
                 print(self.text)
                 time.sleep(1)
                 self._time += 1
                 self.text = time.strftime("%H:%M:%S", time.gmtime(self._time))
 
-            if keyboard.is_pressed("s"):
+            if keyboard.is_pressed(
+                "p"
+            ):  # for some reason i have to hold s for it to work or spam it
                 self.stop()
-                print("Stopwatch stopped")
 
             if keyboard.is_pressed("r"):
                 self.reset()
-                print("Stopwatch reset")
 
             if keyboard.is_pressed("c"):
                 self.resume()
-                print("Stopwatch resumed")
+
+            if keyboard.is_pressed("s"):
+                self.start()
+
+            if keyboard.is_pressed("q"):
+                print("Quitting program")
+                break
 
     def reset(self):
         if not self.isGoing:
+            self.isReset = True
             self._time = 0
             self.text = time.strftime("%H:%M:%S", time.gmtime(self._time))
+            print("Stopwatch reset")
 
 
 stopwatch = Stopwatch()
-
-
-def printit():
-    print("print")
-
 
 ## ^^^^^^^^^^^^^^^^^^^^^^^^^## Stopwatch functions ## ^^^^^^^^^^^^^^^^^^^^^^^^^##
 if __name__ == "__main__":
