ed25096168eef83b91d54faf2160c95966d22d56
CMS2814 <cydhensabate14@gmail.com>
2026-01-08 20:39:47 -0500

    comepleted timer logic basically just modified the stopwatch a bit it's done but i feel like its not complete yet

--- Logic/Timer.py
+++ Logic/Timer.py
@@ -5,15 +5,12 @@ import keyboard
 
 
 ## -------------------------## Stopwatch functions ## -------------------------##
-
-
 class Stopwatch:
     def __init__(self):
-        self._time = 0
+        self.time = 0
         self.isGoing = True
         self.isReset = True
         self.stopWatch = ""
-        self.text = time.strftime("%H:%M:%S", time.gmtime(self._time))
 
     def start(self):
         if self.isReset:
@@ -24,9 +21,8 @@ class Stopwatch:
             print("Stopwatch started")
 
     def stop(self):
-        if self.isGoing:
-            self.isGoing = False
-            print("Stopwatch stopped")
+        self.isGoing = False
+        print("Stopwatch stopped")
         time.sleep(0.1)
 
     def resume(self):
@@ -39,10 +35,10 @@ class Stopwatch:
         curTime = datetime.now()
         while True:
             if self.isGoing:
-                _Time = datetime.now() - curTime
-                if _Time >= timedelta(milliseconds=self._time):
-                    self._time += 10
-                    timetext = timedelta(milliseconds=self._time)
+                TargetTime = datetime.now() - curTime
+                if TargetTime >= timedelta(milliseconds=self.time):
+                    self.time += 10
+                    timetext = timedelta(milliseconds=self.time)
                     print(timetext)
 
             if keyboard.is_pressed("space"):
@@ -60,14 +56,84 @@ class Stopwatch:
     def reset(self):
         if not self.isGoing:
             self.isReset = True
-            self._time = 0
-            self.text = time.strftime("%H:%M:%S", time.gmtime(self._time))
+            self.time = 0
             print("Stopwatch reset")
             time.sleep(0.1)
 
 
-stopwatch = Stopwatch()
-
 ## ^^^^^^^^^^^^^^^^^^^^^^^^^## Stopwatch functions ## ^^^^^^^^^^^^^^^^^^^^^^^^^##
+
+
+## -------------------------## Timer functions ## -------------------------##\
+class Timer:
+    def __init__(self):
+        self.time = 0
+        self.goal = 0
+        self.isGoing = True
+        self.isReset = True
+        self.stopWatch = ""
+        self.text = time.strftime("%H:%M:%S", time.gmtime(self.time))
+
+    def start(self):
+        if self.isReset:
+            while True:
+                try:
+                    goalTime = int(input("Enter goal time in seconds:"))
+                    self.goal = goalTime
+                    break
+                except ValueError:
+                    print("Invalid input. Please enter an integer value.")
+            self.isReset = False
+            self.isGoing = True
+            print("Timer started")
+            self.update()
+
+    def stop(self):
+        if self.isGoing:
+            self.isGoing = False
+            print("Timer stopped")
+            time.sleep(0.1)
+
+    def resume(self):
+        if not self.isGoing:
+            self.isGoing = True
+            print("Stopwatch resumed")
+            time.sleep(0.1)
+
+    def reset(self):
+        if not self.isGoing:
+            self.isReset = True
+            self.time = 0
+            self.goal = 0
+        print("Timer reset")
+
+    def update(self):
+        curTime = datetime.now()
+        while self.goal > 0:
+            if self.isGoing:
+                TargetTime = datetime.now() - curTime
+                if TargetTime >= timedelta(seconds=self.time):
+                    timetext = timedelta(seconds=self.goal)
+                    print(timetext)
+                    self.time += 1
+                    self.goal -= 1
+
+            if keyboard.is_pressed("space"):
+                self.stop()
+            elif keyboard.is_pressed("r"):
+                self.reset()
+            elif keyboard.is_pressed("space"):
+                self.resume()
+            elif keyboard.is_pressed("s"):
+                self.start()
+            elif keyboard.is_pressed("q"):
+                print("Quitting program")
+                break
+        print("Timer finished!")
+
+
+## ^^^^^^^^^^^^^^^^^^^^^^^^^## Timer functions ## ^^^^^^^^^^^^^^^^^^^^^^^^^##
 if __name__ == "__main__":
-    stopwatch.start()
+    stopwatch = Stopwatch()
+    timer = Timer()
+    timer.start()
