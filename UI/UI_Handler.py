ce2f89dd24c6664abadb7f89b4ce3e478d1af0a5
CMS2814 <cydhensabate14@gmail.com>
2026-01-12 11:42:59 -0500

    finally aligned to title to top center T-T

--- UI/UI_Handler.py
+++ UI/UI_Handler.py
@@ -21,27 +21,47 @@ class MainWindow(QMainWindow):
         self.setWindowTitle("Timey")
         self.setGeometry(700, 300, 500, 550)
         # self.mainlabel = QLabel(self)
-        self.mainlayout = QVBoxLayout(self)
         self.mainWidget = QWidget(self)
-        self.currentMode = QLabel("TIMER", self.mainWidget)
+        self.topWidget = QWidget(self.mainWidget)
+        self.midWidget = QWidget(self.mainWidget)
+        self.bottomWidget = QWidget(self.mainWidget)
+        # Labels
+        self.currentMode = QLabel("TIMER", self.topWidget)
+        # Layouts
+        self.mainlayout = QVBoxLayout(self.mainWidget)
+        self.topLayout = QVBoxLayout(self.topWidget)
+        self.midLayout = QVBoxLayout(self.midWidget)
+        self.bottomLayout = QVBoxLayout(self.bottomWidget)
+
         self.initUI()
 
     def initUI(self):
         self.setCentralWidget(self.mainWidget)
+        # Styles
         self.currentMode.setFixedWidth(100)
         self.currentMode.setFixedHeight(50)
         self.currentMode.setAlignment(Qt.AlignmentFlag.AlignCenter)
-        # self.mainlabel.setGeometry(0, 0, self.width(), self.height())
-        # self.mainlabel.setStyleSheet("background-color: red;" "font-size: 40px;")
-        # self.mainlabel.setAlignment(Qt.AlignmentFlag.AlignCenter)
-        # self.mainlayout.addWidget(self.mainlabel)
-        self.mainlayout.addWidget(self.currentMode)
+        self.currentMode.setStyleSheet(
+            "background-color: lightgray; padding: 5px; border-radius: 25px;"
+        )
         self.mainWidget.setStyleSheet("background-color: white;")
-        self.mainWidget.setLayout(self.mainlayout)
+        self.topWidget.setStyleSheet("background-color: red;")
+        self.midWidget.setStyleSheet("background-color: green;")
+        self.bottomWidget.setStyleSheet("background-color: blue;")
+        # self.mainlayout.addWidget(self.currentMode)
+        # Layout properties
+        # Main Layout
+        self.mainlayout.addWidget(self.topWidget)
+        self.mainlayout.addWidget(self.midWidget)
+        self.mainlayout.addWidget(self.bottomWidget)
+        # Top Layout
+        self.topLayout.addWidget(
+            self.currentMode,
+            alignment=Qt.AlignmentFlag.AlignHCenter | Qt.AlignmentFlag.AlignTop,
+        )
+        # Mid Layout
 
-        # self.currentMode.setGeometry(0, 0, 1000, 50)
-        self.currentMode.setStyleSheet("background-color: blue;")
-        self.currentMode.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        # Bottom Layout
 
 
 def main():
