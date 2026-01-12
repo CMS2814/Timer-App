adf1016f42048424704853989c5e96b72e432542
CMS2814 <cydhensabate14@gmail.com>
2026-01-12 18:55:22 -0500

    basic positions

--- UI/UI_Handler.py
+++ UI/UI_Handler.py
@@ -9,8 +9,9 @@ from PyQt6.QtWidgets import (
     QGridLayout,
     QWidget,
     QLabel,
+    QLineEdit,
 )
-from PyQt6.QtGui import QFont
+from PyQt6.QtGui import QFont, QFontDatabase
 from PyQt6.QtCore import QTimer, Qt
 
 
@@ -27,27 +28,41 @@ class MainWindow(QMainWindow):
         self.bottomWidget = QWidget(self.mainWidget)
         # Labels
         self.currentMode = QLabel("TIMER", self.topWidget)
+        self.timeLabel = QLabel("00:00", self.midWidget)
         # Layouts
         self.mainlayout = QVBoxLayout(self.mainWidget)
         self.topLayout = QVBoxLayout(self.topWidget)
         self.midLayout = QVBoxLayout(self.midWidget)
-        self.bottomLayout = QVBoxLayout(self.bottomWidget)
+        self.bottomLayout = QHBoxLayout(self.bottomWidget)
 
         self.initUI()
 
     def initUI(self):
         self.setCentralWidget(self.mainWidget)
         # Styles
+        # Current Mode Label
         self.currentMode.setFixedWidth(100)
         self.currentMode.setFixedHeight(50)
         self.currentMode.setAlignment(Qt.AlignmentFlag.AlignCenter)
         self.currentMode.setStyleSheet(
-            "background-color: lightgray; padding: 5px; border-radius: 25px;"
+            "background-color: lightgray; padding: 5px; border-radius: 5px;"
+            "font-size: 20px;"
         )
+        self.currentMode.setFont(QFont("Arial"))
+        # Widget Labels
         self.mainWidget.setStyleSheet("background-color: white;")
         self.topWidget.setStyleSheet("background-color: red;")
         self.midWidget.setStyleSheet("background-color: green;")
         self.bottomWidget.setStyleSheet("background-color: blue;")
+        # Time Label
+        self.timeLabel.setStyleSheet(
+            "background-color: gray;"
+            "color: black;"
+            "font-size: 65px;"
+            "padding: 10px;"
+        )
+        self.timeLabel.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.timeLabel.setFixedSize(350, 100)
         # self.mainlayout.addWidget(self.currentMode)
         # Layout properties
         # Main Layout
@@ -58,6 +73,10 @@ class MainWindow(QMainWindow):
         self.topLayout.addWidget(
             self.currentMode,
             alignment=Qt.AlignmentFlag.AlignHCenter | Qt.AlignmentFlag.AlignTop,
+        )
+        self.topLayout.addWidget(
+            self.timeLabel,
+            alignment=Qt.AlignmentFlag.AlignHCenter | Qt.AlignmentFlag.AlignBottom,
         )
         # Mid Layout
 
