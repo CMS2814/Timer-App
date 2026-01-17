6ade018a78eaa92facbebb88e2cee975f995495d
CMS2814 <cydhensabate14@gmail.com>
2026-01-17 13:53:07 -0500

    idk

--- Logic/Timer.py
+++ Logic/Timer.py
@@ -68,72 +68,8 @@ class Stopwatch:
 class Timer:
     def __init__(self):
         self.time = 0
-        self.goal = 0
-        self.isGoing = True
-        self.isReset = True
-        self.stopWatch = ""
-        self.text = time.strftime("%H:%M:%S", time.gmtime(self.time))
-
-    def start(self):
-        if self.isReset:
-            while True:
-                try:
-                    goalTime = int(input("Enter goal time in seconds:"))
-                    self.goal = goalTime
-                    break
-                except ValueError:
-                    print("Invalid input. Please enter an integer value.")
-            self.isReset = False
-            self.isGoing = True
-            print("Timer started")
-            self.update()
-
-    def stop(self):
-        if self.isGoing:
-            self.isGoing = False
-            print("Timer stopped")
-            time.sleep(0.1)
-
-    def resume(self):
-        if not self.isGoing:
-            self.isGoing = True
-            print("Stopwatch resumed")
-            time.sleep(0.1)
-
-    def reset(self):
-        if not self.isGoing:
-            self.isReset = True
-            self.time = 0
-            self.goal = 0
-        print("Timer reset")
-
-    def update(self):
-        curTime = datetime.now()
-        while self.goal > 0:
-            if self.isGoing:
-                TargetTime = datetime.now() - curTime
-                if TargetTime >= timedelta(seconds=self.time):
-                    timetext = timedelta(seconds=self.goal)
-                    print(timetext)
-                    self.time += 1
-                    self.goal -= 1
-
-            if keyboard.is_pressed("space"):
-                self.stop()
-            elif keyboard.is_pressed("r"):
-                self.reset()
-            elif keyboard.is_pressed("space"):
-                self.resume()
-            elif keyboard.is_pressed("s"):
-                self.start()
-            elif keyboard.is_pressed("q"):
-                print("Quitting program")
-                break
-        print("Timer finished!")
 
 
 ## ^^^^^^^^^^^^^^^^^^^^^^^^^## Timer functions ## ^^^^^^^^^^^^^^^^^^^^^^^^^##
 if __name__ == "__main__":
-    stopwatch = Stopwatch()
-    timer = Timer()
-    timer.start()
+    pass
