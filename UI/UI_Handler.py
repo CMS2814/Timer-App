da47d3069b8d0e607d59c9ae59543f97361d82b6
CMS2814 <cydhensabate14@gmail.com>
2026-01-17 11:55:35 -0500

    change implemented

--- UI/UI_Handler.py
+++ UI/UI_Handler.py
@@ -21,96 +21,138 @@ class MainWindow(QMainWindow):
 
         self.setWindowTitle("Timey")
         self.setGeometry(700, 300, 500, 550)
-        # self.mainlabel = QLabel(self)
-        self.mainWidget = QWidget(self)
-        self.topWidget = QWidget(self.mainWidget)
-        self.midWidget = QWidget(self.mainWidget)
-        self.bottomWidget = QWidget(self.mainWidget)
-        # Labels
-        self.currentMode = QLabel("TIMER", self.topWidget)
-        self.timeLabel = QLineEdit("00:00", self.midWidget)
 
+        # Main Properties
+        self.mainWidget = QWidget(self)
+
+        self.mainlayout = QVBoxLayout(self.mainWidget)
+
+        # Top Section Properties
+        # /---Widgets
+        self.topWidget = QWidget(self.mainWidget)
+        self.topBarWidget = QWidget(self.topWidget)
+        # /---Buttons/Labels/Etc.
+        self.currentModeTitle = QLabel("TIMER", self.topWidget)
+        self.modeButton = QPushButton("Timer", self.topWidget)
+        self.timeLabel = QLineEdit("00:00", self.topWidget)
+        # /---Layouts
+        self.topLayout = QVBoxLayout(self.topWidget)
+        self.upperTopLayout = QHBoxLayout(self.topBarWidget)
+
+        # Mid Section Properties
+        # /---Widgets
+        self.midWidget = QWidget(self.mainWidget)
+        # /---Buttons/Labels/Etc.
         self.pickTime1 = QPushButton("10m", self.midWidget)
         self.pickTime2 = QPushButton("15m", self.midWidget)
         self.pickTime3 = QPushButton("30m", self.midWidget)
-        # Layouts
-        self.mainlayout = QVBoxLayout(self.mainWidget)
-        self.topLayout = QVBoxLayout(self.topWidget)
+        # /---Layouts
         self.midLayout = QHBoxLayout(self.midWidget)
+
+        # Bottom Section Properties
+        # /---Widgets
+        self.bottomWidget = QWidget(self.mainWidget)
+        # /---Buttons/Labels/Etc.
+        self.startButton = QPushButton("Start", self.bottomWidget)
+        # /---Layouts
         self.bottomLayout = QHBoxLayout(self.bottomWidget)
 
-        self.initUI()
-
-    def initUI(self):
         self.setCentralWidget(self.mainWidget)
-        # Styles
-        # Current Mode Label
-        self.currentMode.setFixedWidth(100)
-        self.currentMode.setFixedHeight(50)
-        self.currentMode.setAlignment(Qt.AlignmentFlag.AlignCenter)
-        self.currentMode.setStyleSheet(
+
+        self.loadLayouts()
+        self.loadStyles()
+        self.checkButtonConnections()
+
+    def loadStyles(self):
+        # -------Sizes-----------------------
+        # Top Widget
+        self.currentModeTitle.setFixedSize(100, 50)
+        self.timeLabel.setFixedSize(350, 100)
+        self.modeButton.setFixedSize(75, 50)
+        # Mid Widget
+        self.pickTime1.setFixedSize(70, 70)
+        self.pickTime2.setFixedSize(70, 70)
+        self.pickTime3.setFixedSize(70, 70)
+        # Bottom Widget
+        self.startButton.setFixedSize(150, 75)
+        # -------Alignments------------------
+        # Top Widget
+        self.currentModeTitle.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.timeLabel.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        # Mid Widget
+        # Bottom Top Widget
+
+        # -------Styles----------------------
+        self.mainWidget.setStyleSheet("background-color: white;")
+        # Top Widget
+        self.topWidget.setStyleSheet("background-color: red;")
+
+        self.currentModeTitle.setStyleSheet(
             "background-color: lightgray; "
             "padding: 5px;"
             "border-radius: 5px;"
             "font-size: 20px;"
         )
-        self.currentMode.setFont(QFont("Arial"))
-        # Widget Labels
-        self.mainWidget.setStyleSheet("background-color: white;")
-        self.topWidget.setStyleSheet("background-color: red;")
-        self.midWidget.setStyleSheet("background-color: green;")
-        self.bottomWidget.setStyleSheet("background-color: blue;")
-        # Time Label
         self.timeLabel.setStyleSheet(
             "background-color: gray;"
             "color: black;"
             "font-size: 65px;"
             "padding: 10px;"
         )
-        self.timeLabel.setAlignment(Qt.AlignmentFlag.AlignCenter)
-        self.timeLabel.setFixedSize(350, 100)
-        # Pick Time Labels
-        timeLabelStyle = (
+
+        self.modeButton.setStyleSheet("background-color: blue; font-size: 20px;")
+        self.currentModeTitle.setFont(QFont("Arial"))
+        # Mid Widget
+        self.midWidget.setStyleSheet("background-color: green;")
+
+        # Pick Time Labels-------
+        pickTimeLabelStyle = (
             "background-color: yellow;"
             "color: black;"
             "font-size: 20px;"
             "border-radius: 35px;"
         )
-        # Time Pick 1
-        self.pickTime1.setStyleSheet(timeLabelStyle)
-        # self.pickTime1.setAlignment(Qt.AlignmentFlag.AlignCenter)
-        self.pickTime1.setFixedSize(70, 70)
-        self.pickTime1.clicked.connect(self.onButtonClick)
-        # Time Pick 2
-        self.pickTime2.setStyleSheet(timeLabelStyle)
-        # self.pickTime2.setAlignment(Qt.AlignmentFlag.AlignCenter)
-        self.pickTime2.setFixedSize(70, 70)
-        self.pickTime2.clicked.connect(self.onButtonClick)
-        # Time Pick 3
-        self.pickTime3.setStyleSheet(timeLabelStyle)
-        # self.pickTime3.setAlignment(Qt.AlignmentFlag.AlignCenter)
-        self.pickTime3.setFixedSize(70, 70)
-        self.pickTime3.clicked.connect(self.onButtonClick)
-        # self.mainlayout.addWidget(self.currentMode)
-        # Layout properties
+        self.pickTime1.setStyleSheet(pickTimeLabelStyle)
+        self.pickTime2.setStyleSheet(pickTimeLabelStyle)
+        self.pickTime3.setStyleSheet(pickTimeLabelStyle)
+        # Bottom Widget
+        self.bottomWidget.setStyleSheet("background-color: blue;")
+
+        self.startButton.setStyleSheet(
+            "background-color: green; color: white;" "font-size: 25px;"
+        )
+
+    def loadLayouts(self):
         # Main Layout
         self.mainlayout.addWidget(self.topWidget)
         self.mainlayout.addWidget(self.midWidget)
         self.mainlayout.addWidget(self.bottomWidget)
         # Top Layout
-        self.topLayout.addWidget(
-            self.currentMode,
-            alignment=Qt.AlignmentFlag.AlignHCenter | Qt.AlignmentFlag.AlignTop,
-        )
+        self.topLayout.addWidget(self.topBarWidget)
+
         self.topLayout.addWidget(
             self.timeLabel,
             alignment=Qt.AlignmentFlag.AlignHCenter | Qt.AlignmentFlag.AlignBottom,
         )
+
+        self.upperTopLayout.addWidget(self.modeButton)
+        self.upperTopLayout.addWidget(
+            self.currentModeTitle, alignment=Qt.AlignmentFlag.AlignLeft
+        )
+        self.upperTopLayout.setSpacing(95)  # default 95
+
         # Mid Layout
         self.midLayout.addWidget(self.pickTime1)
         self.midLayout.addWidget(self.pickTime2)
         self.midLayout.addWidget(self.pickTime3)
         # Bottom Layout
+        self.bottomLayout.addWidget(self.startButton)
+
+    def checkButtonConnections(self):
+        self.pickTime1.clicked.connect(self.onButtonClick)
+        self.pickTime2.clicked.connect(self.onButtonClick)
+        self.pickTime3.clicked.connect(self.onButtonClick)
+        self.startButton.clicked.connect(self.onButtonClick)
 
     def onButtonClick(self):
         print("Button clicked!")
